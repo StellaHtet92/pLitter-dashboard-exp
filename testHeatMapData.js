cb9fd29ab3ce48f9ea08344f582f077bdee86b0c
Stella <htetyaminkokoedu@gmail.com>
2022-05-31 14:35:18 +0700

    setRadius&OpacityOfCFG

--- testHeatMapData.js
+++ testHeatMapData.js
@@ -1,14 +1,23 @@
-var testData = {
-    max: 8,
-    data: [{"longitude":105.7902111,"latitude":10.0042388,"name":"GH010009_0.jpg","count":8,"Plastic":8,"Pile":0,"Face mask":0,"Trash bin":0},
-    {"longitude":105.79020919999999,"latitude":10.0042381,"name":"GH010009_1.jpg","count":9,"Plastic":9,"Pile":0,"Face mask":0,"Trash bin":0}, 
-    {"longitude":105.7902157,"latitude":10.0042419,"name":"GH010009_2.jpg","count":9,"Plastic":9,"Pile":0,"Face mask":0,"Trash bin":0},
-    {"longitude":105.7902473,"latitude":10.0042586,"name":"GH010009_3.jpg","count":8,"Plastic":8,"Pile":0,"Face mask":0,"Trash bin":0},
-    {"longitude":105.79027169999999,"latitude":10.004276,"name":"GH010009_4.jpg","count":2,"Plastic":2,"Pile":0,"Face mask":0,"Trash bin":0},
-    {"longitude":105.7903115,"latitude":10.0043057,"name":"GH010009_5.jpg","count":0,"Plastic":0,"Pile":0,"Face mask":0,"Trash bin":0},
-    {"longitude":105.7903349,"latitude":10.0043244,"name":"GH010009_6.jpg","count":0,"Plastic":0,"Pile":0,"Face mask":0,"Trash bin":0},
-    {"longitude":105.7904202,"latitude":10.0043896,"name":"GH010009_7.jpg","count":0,"Plastic":0,"Pile":0,"Face mask":0,"Trash bin":1},
-    {"longitude":105.7904547,"latitude":10.0044124,"name":"GH010009_8.jpg","count":0,"Plastic":0,"Pile":0,"Face mask":0,"Trash bin":0},
-    {"longitude":105.79050339999999,"latitude":10.0044526,"name":"GH010009_9.jpg","count":2,"Plastic":2,"Pile":0,"Face mask":0,"Trash bin":0},
-    {"longitude":105.790542,"latitude":10.0044844,"name":"GH010009_10.jpg","count":3,"Plastic":3,"Pile":0,"Face mask":0,"Trash bin":1}]
-  };
+function getTestData() {
+  // var heatmap_array = [];
+var geojson = {"max": 1960, "data": []};
+   
+Object.keys(geojsonFeature).forEach((video => {
+  console.log(video); 
+  
+   geojsonFeature[video]["geom"].map((point) => {
+   //console.log('Point from getTestData');
+   //console.log(point);
+       geojson["data"].push({
+           "longitude":point["longitude"],
+           "latitude":point["latitude"],
+           "count": point["count"],            
+          
+      });
+   });
+   console.log('Geojson from getTestData Function');
+   console.log(geojson); 
+  // heatmap_array.push(geojson);
+}))
+return geojson;
+}
